1caf6068c1c50b940042924a1fcea211ba02dc43
Marcos Júnior <marcosajr11@gmail.com>
2024-12-03 23:41:00 -0300

    feat: TaskController class

---
+++ src/main/java/com/todolist/controller/TaskController.java
@@ -0,0 +1,15 @@
+package com.todolist.controller;
+
+import com.todolist.model.Task;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class TaskController {
+
+    private List<Task> tasks = new ArrayList<>();
+
+
+}
