d664c774d7c70110a5ef402ed14f5b25714a5d9c
Marcos Júnior <marcosajr11@gmail.com>
2024-12-05 19:37:56 -0300

    feat: Register new tasks and showing

--- src/main/java/com/todolist/controller/TaskController.java
+++ src/main/java/com/todolist/controller/TaskController.java
@@ -2,6 +2,10 @@ package com.todolist.controller;
 
 import com.todolist.model.Task;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,5 +15,22 @@ public class TaskController {
 
     private List<Task> tasks = new ArrayList<>();
 
+    @GetMapping("/tasks")
+    public String listTasks(Model model) {
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
 
+    @GetMapping("/register-tasks")
+    public String showTaskForm(Model model) {
+        model.addAttribute("task", new Task());
+        return "register-tasks";
+    }
+
+    @PostMapping("/register-tasks")
+    public String registerTask(@ModelAttribute Task task, Model model) {
+        task.setComplete(false);
+        tasks.add(task);
+        return "redirect:/tasks";
+    }
 }
