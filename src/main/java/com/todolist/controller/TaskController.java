89f0e15907087c85fb36d33316b060053cb0603a
Marcos Júnior <marcosajr11@gmail.com>
2024-12-12 21:14:39 -0300

    feat: TaskController now as REST Controller

--- src/main/java/com/todolist/controller/TaskController.java
+++ src/main/java/com/todolist/controller/TaskController.java
@@ -1,36 +1,60 @@
 package com.todolist.controller;
 
 import com.todolist.model.Task;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
 
     private List<Task> tasks = new ArrayList<>();
 
-    @GetMapping("/tasks")
-    public String listTasks(Model model) {
-        model.addAttribute("tasks", tasks);
-        return "tasks";
+    @GetMapping()
+    public List<Task> getAllTasks() {
+        return tasks;
     }
 
-    @GetMapping("/register-tasks")
-    public String showTaskForm(Model model) {
-        model.addAttribute("task", new Task());
-        return "register-tasks";
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable int id){
+        for (Task task: tasks) {
+            if (task.getId() == id) {
+                return task;
+            }
+        }
+        return null;
     }
 
-    @PostMapping("/register-tasks")
-    public String registerTask(@ModelAttribute Task task, Model model) {
-        task.setComplete(false);
+    @PostMapping()
+    public Task createTask(@RequestBody Task task) {
+        int id = tasks.size() + 1;
+        task.setId(id);
         tasks.add(task);
-        return "redirect:/tasks";
+        return task;
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable int id, @RequestBody Task updatedTask) {
+        for (Task task : tasks) {
+            if (task.getId() == id) {
+                task.setDescription(updatedTask.getDescription());
+                task.setComplete(updatedTask.getComplete());
+                return task;
+            }
+        }
+        return null;
+    }
+    @DeleteMapping("/{id}")
+    public boolean deleteTask(@PathVariable int id) {
+        for (Task task : tasks) {
+           if (task.getId() == id) {
+               tasks.remove(task);
+               return true;
+           }
+        }
+        return false;
     }
 }
